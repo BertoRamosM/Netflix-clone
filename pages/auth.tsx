0552688bb4ca804779bd40f168e7246162616501
ARamos <albertramos902@gmail.com>
2024-09-08 14:42:56 +0000

    setting up auth page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+
 const Auth = () => {
   return (
     <div className="relative h-full w-full bg-[url('/images/header.jpg')] bg-cover bg-center">
-        <div className="bg-black w-full h-full lg:bg-opacity-50"></div>
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <Image src={"/images/logo.svg"} width={100} height={100} alt="logo" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-1/2 lg:max-w-md rounded-md h-full">
+
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 };
