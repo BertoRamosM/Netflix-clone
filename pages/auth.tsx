248dbf7c656e1961fd237e6f964a1db346f16560
ARamos <albertramos902@gmail.com>
2024-09-08 14:38:50 +0000

    working on header styles

---
+++ pages/auth.tsx
@@ -0,0 +1,9 @@
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/header.jpg')] bg-cover bg-center">
+        <div className="bg-black w-full h-full lg:bg-opacity-50"></div>
+    </div>
+  );
+};
+
+export default Auth;
