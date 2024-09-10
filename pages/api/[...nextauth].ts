9deb977893a79f4f064a3b3685df6048439f7a04
ARamos <albertramos902@gmail.com>
2024-09-10 08:52:13 +0000

    finished next auth setup with bcrypt and bcrypt types

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -1,7 +1,59 @@
+import client from "@/lib/prisma.db";
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import {compare} from "bcrypt"
 
 export default NextAuth({
   providers: [
-    
-  ]
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email ||
+          !credentials?.password) { 
+          throw new Error("Email or password required");
+        }
+
+        const user = await client.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error("Email dosn't exist");
+        }
+
+        const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+
+        if(!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
+      }
+    })
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 })
